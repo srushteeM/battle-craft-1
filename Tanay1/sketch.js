396e7f265ecd2ce384635c4920e193f374aa6a77
srushteeM <59560681+srushteeM@users.noreply.github.com>
2020-09-14 20:02:28 +0530

    Add files via upload

---
+++ Tanay1/sketch.js
@@ -0,0 +1,56 @@
+
+var heart1,heart2,player1,player1_img,player2,ground,weapon;
+var bg,weapon_img;
+
+
+function preload(){
+bg=loadImage("photos/background.png");
+player1_img=loadImage("photos/1noweapons.png");
+weapon_img=loadImage("photos/weapon1.PNG");
+}
+
+function setup() {
+  var canvas = createCanvas(1200,400);
+    
+    ground=createSprite(600,410,1200,20);
+    ground.visible=false;
+
+    player1=createSprite(100,325,20,20);
+    player1.addImage(player1_img);
+    player1.scale=0.5;
+
+    weapon=createSprite(player1.x+50,player1.y-35);
+    weapon.addImage(weapon_img);
+    weapon.scale=0.2
+
+
+}
+function draw() {
+  background(bg);  
+
+weapon.x=player1.x+50;
+weapon.y=player1.y-35;
+if(keyDown(UP_ARROW)&&player1.y>=327){
+  player1.velocityY=-16;
+}
+
+player1.velocityY+=0.7;
+
+
+player1.collide(ground);
+
+if(keyDown(LEFT_ARROW)){
+  player1.x-=5;
+}
+if(keyDown(RIGHT_ARROW)){
+  player1.x+=5;
+}
+ellipseMode(RADIUS);
+stroke("red");
+strokeWeight(10);
+fill("red");
+ellipse(player1.x,player1.y,30,50)
+  drawSprites();
+
+  
+}
